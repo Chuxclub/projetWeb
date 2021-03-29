33fdc444695f18599ac128eb8027114f16b1954c
Florian <florian-1992@hotmail.fr>
2021-03-29 12:18:35 +0200

    Le site a tout pour fonctionner

---
+++ codes/src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @package App\Controller
+ * @Route("/main")
+ */
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("", name="main_index")
+     */
+    public function indexAction(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
