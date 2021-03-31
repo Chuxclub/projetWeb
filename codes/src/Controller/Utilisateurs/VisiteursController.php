590bc7af68e9d60a49ae4d02a4304aca9a461119
frafraA <amandine.fradet@live.fr>
2021-03-31 23:51:23 +0200

    creation du compte formulaire en place mais il n'y a pas d'ajout dans la base

--- codes/src/Controller/Utilisateurs/VisiteursController.php
+++ codes/src/Controller/Utilisateurs/VisiteursController.php
@@ -4,9 +4,14 @@
 namespace App\Controller\Utilisateurs;
 
 
+use App\Entity\Utilisateurs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Form\CreateAccountType;
 
 /**
  * @package App\Controller
@@ -15,10 +20,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class VisiteursController extends AbstractController
 {
     /**
-     * @Route("/create", name="create")
+     * @Route("/createAccount", name="create_account")
      */
-    public function createAccountAction(): Response
+    public function createAccountAction(Request $request): Response
     {
-        return $this->render('create_customer_account.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $newUser = new Utilisateurs();
+
+        $form = $this->createForm(CreateAccountType::class, $newUser);
+        $form->add('send', SubmitType::class, ['label' => 'create account']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+                $em->flush();
+                $this->addFlash('info', "New account created");
+                return $this->redirectToRoute('main_index');
+        }
+
+        if ($form->isSubmitted())
+            $this->addFlash('info', 'Form not correct');
+
+        $args = array('myform' => $form->createView());
+        return $this->render('Utilisateurs/Visiteur/create_customer_account.html.twig', $args);
+    }
+
+    /**
+     * @Route(
+     *     "/connexion",
+     *     name="utilisateurs_connexion"
+     * )
+     */
+    public function disconnectAction(): Response
+    {
+        $this->addFlash('info', "You will soon be able to connect !");
+        return $this->redirectToRoute("main_index");
     }
 }
---
+++ codes/src/Form/CreateAccountType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateurs;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login')
+            ->add('mdp', PasswordType::class, ['label' => 'Mot de Passe', 'always_empty' => false])
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateN',DateType::class, ['label' => 'Date de Naissance'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateurs::class,
+        ]);
+    }
+}
